dbd0452f1ab88d2938dd0b9268c09a163340905c
noogler-eng <sharadpoddar10102003@gmail.com>
2024-10-26 01:16:38 +0530

    animatedBeam

--- src/components/ui/animated-beam.tsx
+++ src/components/ui/animated-beam.tsx
@@ -95,6 +95,7 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
     const resizeObserver = new ResizeObserver((entries) => {
       // For all entries, recalculate the path
       for (let entry of entries) {
+        console.log(entry);
         updatePath();
       }
     });
